6012fecd0a568b8643a4bc16bef1849b6e4de22f
John D. Corless <jdcorless@gmail.com>
2015-05-08 17:03:18 -0500

    uncommented lines to make script run on new machine

--- emacs_install.sh
+++ emacs_install.sh
@@ -6,11 +6,11 @@
 #sudo apt-get update
 sudo apt-get build-dep emacs24
 sudo apt-get install build-essential
-#sudo apt-get install build-dep emacs
+sudo apt-get install build-dep emacs
 cd ~/Downloads
-#wget http://ftp.gnu.org/gnu/emacs/emacs-24.4.tar.gz
-#gunzip emacs-24.4.tar.gz
-#tar xvf emacs-24.4.tar
+wget http://ftp.gnu.org/gnu/emacs/emacs-24.4.tar.gz
+gunzip emacs-24.4.tar.gz
+tar xvf emacs-24.4.tar
 cd emacs-24.4
 ./configure
 make
