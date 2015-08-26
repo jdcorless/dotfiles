9717259e6be6945c35623bde26dbbc92e0ea6fd9
John D. Corless <jdcorless@gmail.com>
2015-08-26 18:18:48 -0500

    Add two scripts from Matthew McCullough

---
+++ generaterandomfiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# From https://github.com/matthewmccullough/scripts/blob/master/generaterandomfiles
+
+#Ensure we have the quantity specified on the CLI
+if [ -z "$3" ]; then ARG_ERR=ERR; fi
+if [ -z "$2" ]; then ARG_ERR=ERR; fi
+if [ -z "$1" ]; then ARG_ERR=ERR; fi
+if [ -n "$ARG_ERR" ];
+then
+    echo "Usage: <filecount> <filenamebase> <filenameextension>"
+    exit
+fi
+
+count=$1
+filenamebase=$2
+filenameextension=$3
+for (( filenumber = 1; filenumber <= $count ; filenumber++ )); do echo "Some random text: $RANDOM" > $filenamebase$filenumber.$filenameextension; done
