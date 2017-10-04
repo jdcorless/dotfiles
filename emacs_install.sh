2a50e650ccb0f7e4a479b2aa754ff58ec6015c7f
John D. Corless <jdcorless@gmail.com>
2017-10-04 13:35:36 -0500

    Update emacs install for version 25.3

--- emacs_install.sh
+++ emacs_install.sh
@@ -8,10 +8,10 @@ sudo apt-get build-dep emacs24
 sudo apt-get install build-essential
 sudo apt-get install build-dep emacs
 cd ~/Downloads
-wget http://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz
-gunzip emacs-24.5.tar.gz
-tar xvf emacs-24.5.tar
-cd emacs-24.5
+wget http://ftp.gnu.org/gnu/emacs/emacs-25.3.tar.gz
+gunzip emacs-25.3.tar.gz
+tar xvf emacs-25.3.tar
+cd emacs-25.3
 ./configure
 make
 sudo make install
